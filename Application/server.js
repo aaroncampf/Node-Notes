a3a65b31725a7a42d0cabeaf78a36c0c657eceb0
Aaron Campf <aaroncampf@gmail.com>
2016-12-04 18:34:45 -0800

    Added Index.hbs

--- Application/server.js
+++ Application/server.js
@@ -13,11 +13,7 @@ var Companies = require("./Database/Data.json").Companies;
 // var Products: any = require("./Database/Data.json").Products;
 // todo: Figure out the correct type for res
 app.get("/", function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Home Page</h1>\n");
-    res.write("<a href='/Companies'>Companies</h1> <br/>");
-    res.write("<a href='/Companies/1'>AJP Northwest</h1>\n");
-    res.end("");
+    res.render("Index");
 });
 var companyRouter = Express.Router();
 companyRouter.get("/", function (req, res) {
